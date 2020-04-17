dbf8cff7ba65447ec6dd252bf91ad8a1c1b3f290
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 14:48:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,15 @@
+# def reverse_each_word(sentence)
+#   reversed = []
+#   reversed_sencence = ""
+#   (sentence.split).each do|word|
+#     reversed << word.reverse
+#   end
+#   return reversed.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  reversed = []
-  reversed_sencence = ""
-  (sentence.split).each do|word|
-    reversed << word.reverse
+  (sentence.split).collect do|word|
+    word.reverse
   end
-  return reversed.join(" ")
+  return sentence.join(" ")
 end
